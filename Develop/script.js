cd8fc3babe388e78ae3d4b1b34b629fadeb378d7
lizbethcastro <116051273+lizbethcastro@users.noreply.github.com>
2023-03-12 23:05:25 -0600

    Added password length confirmations

--- Develop/script.js
+++ Develop/script.js
@@ -4,9 +4,21 @@ var upperCase = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'
 var lowerCase = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
 var special = ('!','@','#','$','%','^','&','*','(',')','-','_','=','+',"[",'{',']','}',';',':',"'",'"','|',',','<',".",'>','/','?');
 
-
 function generatePassword() {
+  var acceptCharc = [];
+  var passwordLength = window.prompt("From 8-128 characters, how long do you want your password to be?");
 
+// Password Length Confirmation 
+    if (passwordLength < 8) {
+      window.alert ("Please select a higher number")
+    } 
+    else if (passwordLength > 128) {
+      window.alert ("Please select a lower number")
+    }
+    else if (isNaN(passwordLength)) {
+      window.alert ("Please choose a number.")
+    }
+  
 }
 
 // Assignment Code
