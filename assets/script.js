a9634ce65f2e8bf6dd5ac846268155ed60cd145b
lizbethcastro <116051273+lizbethcastro@users.noreply.github.com>
2023-03-16 20:32:07 -0600

    Fixed arrays

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 // Acceptable charcters.
-var numbers = ('0','1','2','3','4','5','6','7','8','9');
-var upperCase = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-var lowerCase = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-var special = ('!','@','#','$','%','^','&','*','(',')','-','_','=','+',"[",'{',']','}',';',':',"'",'"','|',',','<',".",'>','/','?');
+var numbers = ["0","1","2","3","4","5","6","7","8","9","10"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var special = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}",";",":","'","|",",","<",".",">","/","?"];
 
 function generatePassword() {
   var acceptPassword = [];
@@ -45,15 +45,15 @@ function generatePassword() {
     }
 
   console.log(acceptPassword);
-  
+
 // For-Loops (Took directly from activity 13 and mini-project)
-  password = "";
+  var password = "";
   for (var i = 0; i < passLength; i++) {
     var randomPass = Math.floor(Math.random() * acceptPassword.length);
     password += acceptPassword[randomPass];
-  
   }
-  return password;
+  
+  return password
 }
 
 // Assignment Code
